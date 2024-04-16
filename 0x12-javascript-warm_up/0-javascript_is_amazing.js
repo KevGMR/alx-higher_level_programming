efbefde451fb167f1bf9abafb0cd4139af9680aa
Kevin Gatamu <kevingatamumuthoni@gmail.com>
2024-04-16 18:19:52 +0300

    update: node

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,7 +1,3 @@
 #!/usr/bin/node
-<<<<<<< HEAD
 const myVar = 'Javascript is amazing';
-=======
-const myVar = 'JavaScript is amazing';
->>>>>>> 0f5a491f5d4a35150e938307a3c8b58d1ff63c3a
 console.log(myVar);
