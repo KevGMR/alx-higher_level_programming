2273a3c81eef1d579efd06d69b8e6777232c8bef
Kevin Gatamu <kevingatamumuthoni@gmail.com>
2024-04-17 18:12:50 +0300

    update  0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -2,15 +2,15 @@
 """Lists states from database"""
 
 import MySQLdb
-from sys import argv
+import sys
 
 if __name__ == "__main__":
     """
     Checks if the file being executed is the main file.
 
     """
-    conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                           passwd=argv[2], db=argv[3], charset="utf8")
+    conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                           passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
     cur = conn.cursor()
     cur.execute("SELECT * FROM states ORDER BY states.id ASC")
     query_rows = cur.fetchall()
