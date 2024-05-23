773cf2b0c5b812142abfa20792ca38bd9c0cca1e
kevin(parrot) <kevingatamumuthoni@gmail.com>
2024-05-23 18:44:36 +0300

    ran semistandard

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-const request = require("request");
+const request = require('request');
 const episodeNum = process.argv[2];
-const API_URL = "https://swapi-api.alx-tools.com/api/films/";
+const API_URL = 'https://swapi-api.alx-tools.com/api/films/';
 
 request(API_URL + episodeNum, function (err, response, body) {
   if (err) {
@@ -10,6 +10,6 @@ request(API_URL + episodeNum, function (err, response, body) {
     const responseJSON = JSON.parse(body);
     console.log(responseJSON.title);
   } else {
-    console.log("Error code: " + response.statusCode);
+    console.log('Error code: ' + response.statusCode);
   }
 });
