eaed8730f9a36a47dea8c1571db1d011d44c5dd3
kevin(parrot) <kevingatamumuthoni@gmail.com>
2024-05-30 15:55:50 +0300

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     argvLength = len(argv)
 
     if (argvLength == 1):
-        print("{:d} arguments".format(argvLength - 1))
+        print("{:d} arguments.".format(argvLength - 1))
     if (argvLength == 2):
         print("{:d} argument:".format(argvLength - 1))
     if (argvLength > 2):
