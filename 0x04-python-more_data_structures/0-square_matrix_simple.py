61692149a39b4b6820d257e6bb695dba6b976aa9
Kevin Gatamu <kevingatamumuthoni@gmail.com>
2024-06-13 23:55:19 +0300

    0-square_matrix_simple.py

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    new_matrix = [[0 for _ in row] for row in matrix]
-    for i in range(len(matrix)):
-        for j in range(len(matrix[i])):
-            new_matrix[i][j] = matrix[i][j] ** 2
+    new_matrix = []
+    for i in matrix:
+        inner_matrix = []
+        for j in i:
+            inner_matrix.append(j*j)
+        new_matrix.append(inner_matrix)
+
     return new_matrix
