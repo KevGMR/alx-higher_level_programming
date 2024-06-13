192ee14cefc2f3aee0c7aa53abd2be67ca8ebf23
Kevin Gatamu <kevingatamumuthoni@gmail.com>
2024-06-13 23:23:19 +0300

    11-delete_at.py_trial_2

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-    new_list = []
 
-    for index, i in enumerate(my_list):
-        if index != idx:
-            new_list.append(i)
+    if (idx > 0) and idx < len(my_list):
+        del my_list[idx]
 
-    return new_list
+    return my_list
