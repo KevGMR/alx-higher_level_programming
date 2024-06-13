13336436fce1521fd45055da5898eca5bd7fcf56
Kevin Gatamu <kevingatamumuthoni@gmail.com>
2024-06-13 21:33:08 +0300

    6-print_matrix_integer.py

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for i in matrix:
-        for j in i:
-            print("{}".format(j), end=" ")
-        print(" ", end="\n")
+    for row in matrix:
+        print(" ".join("{}".format(j) for j in row))
