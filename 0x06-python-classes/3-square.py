a9a69c3edf3bcc25e281ab64d2949ec82de058b7
Kevin_WORK <kevingatamumuthoni@gmail.com>
2024-06-17 17:39:22 +0300

    3-square.py

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+"""Defines Class Square"""
+
 
 class Square:
+    """Defines instance of class"""
     def __init__(self, size=0):
         if isinstance(size, int) is not True:
             raise TypeError("size must be an integer")
