99e6434db0e2ea180449ceeb58cd2cc58e2636d1
Kevin_WORK <kevingatamumuthoni@gmail.com>
2024-06-17 18:02:55 +0300

    5-square.py

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""Defines Class Square"""
+
+
+class Square:
+    """Defines instance of class"""
+    def __init__(self, size=0):
+        if isinstance(size, int) is not True:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
+
+    @property
+    def size(self):
+        """Getter to retrieve size of square"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Setter to update size"""
+        if isinstance(value, int) is not True:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = value
+
+    def area(self):
+        """Returns area of square"""
+        return self.__size * self.__size
+
+    def my_print(self):
+        """Prints the square"""
+        if self.__size == 0:
+            print()
+        else:
+            for _ in range(self.__size):
+                print("#" * self.__size)
