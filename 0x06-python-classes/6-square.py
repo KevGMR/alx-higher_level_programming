72837bc618d23634866a5c4c3bf59a3450c37d4d
Kevin_WORK <kevingatamumuthoni@gmail.com>
2024-06-17 18:36:45 +0300

    6-square.py_2

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -49,11 +49,10 @@ class Square:
     def my_print(self):
         """Prints the square"""
         if self.__size == 0:
-            print()
-            return
-
-        [print("") for _ in range(0, self.__position[1])]
-        for _ in range(0, self.__size):
-            [print(" ", end="") for _ in range(0, self.__position[0])]
-            [print("#", end="") for _ in range(0, self.__size)]
             print("")
+        else:
+            [print("") for _ in range(0, self.__position[1])]
+            for _ in range(0, self.__size):
+                [print(" ", end="") for _ in range(0, self.__position[0])]
+                [print("#", end="") for _ in range(0, self.__size)]
+                print("")
