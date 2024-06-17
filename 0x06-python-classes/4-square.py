6e3c62a0facba6db04e6394b33afa83123fc2902
Kevin_WORK <kevingatamumuthoni@gmail.com>
2024-06-17 17:48:13 +0300

    4-square.py

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Defines Class Square"""
+
+
+class Square:
+    """Defines instance of class"""
+    def __init__(self, size=0):
+        if isinstance(size, int) is not True:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
+
+    @property
+    def size(self):
+        """Getter to retrieve size of square"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Setter to update size"""
+        if isinstance(value) is not True:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = value
+
+    def area(self):
+        """Returns area of square"""
+        return self.__size * self.__size
