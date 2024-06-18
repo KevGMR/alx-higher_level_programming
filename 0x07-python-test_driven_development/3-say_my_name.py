fa357f5db8bc6b32280c62f93c3ce409be594671
Kevin_WORK <kevingatamumuthoni@gmail.com>
2024-06-18 16:08:18 +0300

    tests

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -9,9 +9,9 @@ def say_my_name(first_name, last_name=""):
     Function for string concatenation
     """
 
-    if not isinstance(first_name, str) and not first_name:
+    if not isinstance(first_name, str) or not first_name:
         raise TypeError("first_name must be a string")
-    if not isinstance(last_name, str) and not last_name:
+    if not isinstance(last_name, str) or not len(last_name) >= 0:
         raise TypeError("last_name must be a string")
 
     print(f"{first_name} {last_name}")
