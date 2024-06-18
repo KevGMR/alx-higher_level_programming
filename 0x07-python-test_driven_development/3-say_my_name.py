8892ec8784f596b983fa49b93db23cf85cb2eaf4
Kevin_WORK <kevingatamumuthoni@gmail.com>
2024-06-18 16:15:58 +0300

    removed f-string

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -14,4 +14,4 @@ def say_my_name(first_name, last_name=""):
     if not isinstance(last_name, str) or not len(last_name) >= 0:
         raise TypeError("last_name must be a string")
 
-    print(f"My name is {first_name} {last_name}")
+    print("My name is {} {}".format(first_name, last_name))
