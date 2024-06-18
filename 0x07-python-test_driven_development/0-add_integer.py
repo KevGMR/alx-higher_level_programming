12cafa2c6af16971a74e117236a22ddc7218b1a4
Kevin_WORK <kevingatamumuthoni@gmail.com>
2024-06-18 11:54:40 +0300

    0-add_integer.py

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -7,8 +7,9 @@ integers
 
 
 def add_integer(a, b=98):
-    """Function raises error if not int or float
-        converts floats to int if exceptions pass
+    """
+        Function raises error if not int or \
+        float converts floats to int if exceptions pass
     """
     if not isinstance(a, float) and not isinstance(a, int):
         raise TypeError("a must be an integer")
