4b48397aac635b0a53690f9892720fbebf27eb08
Kevin_WORK <kevingatamumuthoni@gmail.com>
2024-06-18 14:27:26 +0300

    none

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -9,32 +9,30 @@ def matrix_divided(matrix, div):
     Function that divides all in matrix
     """
 
+    error = "matrix must be a matrix (list of lists) of integers/floats"
+
     if not matrix:
-        raise TypeError("matrix must be a matrix "
-                        "(list of lists) of integers/floats")
+        raise TypeError(error)
     if not isinstance(matrix, list):
-        raise TypeError("matrix must be a matrix "
-                        "(list of lists) of integers/floats")
+        raise TypeError(error)
+    if not isinstance(div, float) and not isinstance(div, int):
+        raise TypeError("div must be a number")
 
     result = []
     matrix_len = len(matrix[0])
 
-    if not isinstance(div, float) and not isinstance(div, int):
-        raise TypeError("div must be a number")
     for i in matrix:
-
         if len(i) > matrix_len:
             raise TypeError("Each row of the matrix must have the same size")
 
         inner_result = []
         for j in i:
             if not isinstance(j, float) and not isinstance(j, int):
-                raise TypeError("matrix must be a matrix (list of lists) of"
-                                " integers/floats")
+                raise TypeError(error)
             if j == 0:
                 raise ZeroDivisionError("division by zero")
 
-            inner_result.append(float("{:.2f}".format(j/div)))
+            inner_result.append(round(j / div, 2))
         result.append(inner_result)
 
     return result
