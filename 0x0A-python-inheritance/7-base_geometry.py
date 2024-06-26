9409fde5be2dba3fd2a36c2b8939d64254fd5e6f
Kevin Gatamu <kevingatamumuthoni@gmail.com>
2024-06-26 16:36:14 +0300

    tests_v2

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -6,13 +6,18 @@ Contains the class BaseGeometry
 
 class BaseGeometry:
     """An empty class"""
+
     def area(self):
-        """raises an exception"""
+        """
+        raises an exception
+        """
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """valiidates value"""
-        if not isinstance(value, int):
+        """
+        validates value
+        """
+        if type(value) is not int:
             raise TypeError("{:s} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{:s} must be greater than 0".format(name))
