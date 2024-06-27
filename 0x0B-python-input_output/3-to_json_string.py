c3d285e4d1a1015cb6dc306f5a73fcb3fd0fabad
Kevin Gatamu <kevingatamumuthoni@gmail.com>
2024-06-27 11:32:05 +0300

    python json.stringify

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""contains the to json string function"""
+import json
+
+
+def to_json_string(my_obj):
+    """converts json to string(json stringify)"""
+    return json.dumps(my_obj)
