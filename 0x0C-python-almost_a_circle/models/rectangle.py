64db720ca6c0315a53cb686dd3c765465fbb130f
Kevin_WORK <kevingatamumuthoni@gmail.com>
2024-06-28 15:16:07 +0300

    rectangle module + tests

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+"""
+Contains class Rectangle
+It inherits Base (check directory)
+"""
+
+from models.base import Base
+
+
+class Rectangle(Base):
+    """Inits rectangle instance"""
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """initialization with id"""
+        super().__init__(id)
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
+
+    @property
+    def width(self):
+        """width getter"""
+        return self.__width
+
+    @property
+    def height(self):
+        """height getter"""
+        return self.__height
+
+    @property
+    def x(self):
+        """x getter"""
+        return self.__x
+
+    @property
+    def y(self):
+        """y getter"""
+        return self.__y
+
+    @width.setter
+    def width(self, value):
+        self.__width = value
+
+    @height.setter
+    def height(self, value):
+        self.__height = value
+
+    @x.setter
+    def x(self, value):
+        self.__x = value
+
+    @y.setter
+    def y(self, value):
+        self.__y = value
