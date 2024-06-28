4a0ab6e0bd8ba8a7d40dec599dbaa762fd64193c
Kevin Gatamu <kevingatamumuthoni@gmail.com>
2024-06-28 23:09:16 +0300

    setter and getter for size based on width and height

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -14,8 +14,18 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         """initialization"""
         super().__init__(size, size, x, y, id)
-        self.size = size
+
+    @property
+    def size(self):
+        """getter for size"""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """setter for square size"""
+        self.width = value
+        self.height = value
 
     def __str__(self):
         return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y,
-                                                 self.size)
+                                                 self.width)
